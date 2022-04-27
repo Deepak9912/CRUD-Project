ac6d1d546bc7de301db999af83ce9bd47deffa70
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-04-27 20:28:31 +0000

    add edit_category.html file

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -12,16 +12,21 @@ def home():
 def categories():
     categories = list(Category.query.order_by(Category.category_name).all())
     return render_template("categories.html", categories=categories)
-    # above first category is html file name 
-    # second category is variable in the function above.
 
 
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
     if request.method == "POST":
-        # below code will target the category
         category = Category(category_name=request.form.get("category_name"))
         db.session.add(category)
         db.session.commit()
         return redirect(url_for("categories"))
     return render_template("add_category.html")
+
+
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    if request.method == 'POST':
+        pass
+    return render_template("edit_category.html")
